132e028e135fcd3ab4580911210aa87ab226740c
Julius Paffrath <github@julius-paffrath.de>
2026-08-09 21:34:11 +0200

    Fix #6

--- Interpreter/Interpreter.InternalFunctions.Modules.cs
+++ Interpreter/Interpreter.InternalFunctions.Modules.cs
@@ -38,14 +38,91 @@ public partial class Interpreter
 
         Interpreter module = _modules[moduleName];
         StringBuilder builder = new StringBuilder();
-        
-        foreach (var func in module._functions)
-        {
-            builder.Append(func.Key + "\n");
-        }
 
-        // remove last \n from string
-        builder.Remove(builder.Length - 1, 1);
+        builder.Append("--- Loaded modules ---\n");
+        if (module._modules.Count() == 0)
+        {
+            builder.Append('/');
+        }
+        else
+        {
+            foreach (var modl in module._modules)
+            {
+                builder.Append(modl.Key + "\n");
+            }
+
+            // remove last \n from string
+            builder.Remove(builder.Length - 1, 1);
+        }
+        builder.Append("\n");
+
+        builder.Append("--- Struct definitions ---\n");
+        if (module._structs.Count() == 0)
+        {
+            builder.Append('/');
+        }
+        else
+        {
+            foreach (var str in module._structs)
+            {
+                builder.Append(str.Key + "\n");
+            }
+
+            // remove last \n from string
+            builder.Remove(builder.Length - 1, 1);
+        }
+        builder.Append("\n");
+
+        builder.Append("--- Function definitions ---\n");
+        if (module._functions.Count() == 0)
+        {
+            builder.Append('/');
+        }
+        else
+        {
+            foreach (var func in module._functions)
+            {
+                builder.Append(func.Key.Split('(')[0]);
+                builder.Append("(");
+
+                for (int i = 0; i < func.Value.Params.Count; i++)
+                {
+                    var param = func.Value.Params[i];
+
+                    // we do not want to evaluate every possible expression here... so only showing literals
+                    string? def = param.Default switch
+                    {
+                        Expression.Literal lit => Stringify(lit.Value),
+                        Expression.Variable => "*variable*",
+                        Expression.Unary => "*expression*",
+                        Expression.Binary => "*expression*",
+                        Expression.Grouping => "*expression*",
+                        Expression.Call => "*function call*",
+                        Expression.NamedCall => "*function call*",
+                        Expression.ModuleCall => "*function call*",
+                        Expression.ModuleNamedCall => "*function call*",
+                        Expression.StructCall => "*expression*",
+                        Expression.MemberAccess => "*expression*",
+                        Expression.MapLiteral => "*expression*",
+                        Expression.MapIndex => "*expression*",
+                        Expression.ListLiteral => "*expression*",
+                        _ => null
+                    };
+
+                    builder.Append(param.Name.Lexeme + ": " + param.Type.Lexeme + (def != null ? " = " + def : ""));
+
+                    if (i != func.Value.Params.Count - 1)
+                    {
+                        builder.Append(", ");
+                    }
+                }
+
+                builder.Append(")\n");
+            }
+
+            // remove last \n from string
+            builder.Remove(builder.Length - 1, 1);
+        }
 
         return builder.ToString();
     }
